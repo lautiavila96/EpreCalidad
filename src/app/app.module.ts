f5fcee7053874e97488f34e02068793a5ff8f7f0
Federico Douglas Price <fdouglas@eprern.gov.ar>
2023-07-28 14:30:04 -0300

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+ import {MatToolbarModule} from '@angular/material/toolbar';
+import { ToolbarComponent } from './toolbar/toolbar.component'; 
+import {MatIconModule} from '@angular/material/icon'; 
+
+/* MODULOS MATERIAL */
+import {MatButtonModule} from '@angular/material/button';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+
+
+/* MODULOS PRIME NG */
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { AccordionModule } from 'primeng/accordion';
+import { AcordeonComponent } from './sidenav/acordeon/acordeon.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidenavComponent,
+    AcordeonComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    ButtonModule,
+    SidebarModule,
+    MatSidenavModule,
+    AccordionModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SidenavComponent } from '../sidenav/sidenav.component';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css'],
+
+})
+export class ToolbarComponent {
+  
+
+}
